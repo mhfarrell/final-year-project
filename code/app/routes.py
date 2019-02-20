0030fdd229c8bc166475880980fb1b70e90c09ff
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2019-02-20 17:44:03 +0000

    building login
    added login code

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,12 +1,40 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session
 from app import app
-import requests
-import json
+import bcrypt
 import sys, getopt, pprint
 from pymongo import MongoClient
 
+c = MongoClient('mongodb://admin:Admin123@ds119024.mlab.com:19024/chatdatabase')
+db= c.chatdatabase
+
 @app.route('/')
 @app.route('/index')
+def index():
+    if 'username' in session:
+        return render_template('index.html')
+    
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = db.users
+    loginUser = users.find_one({'username' : request.form['username']})
+    hashPass = bcrypt.hashpw(request.form['password'].encode('utf-8'), loginUser['password'])
+    if loginUser:
+        if hashPass == loginUser['password']:
+            session['username'] = request.form['username']
+            return render_template('index.html')
+    return render_template('login.html' loginMessage='Invalid Password/Username')
+
+@app.route('/logout')
+def index():
+    return render_template('login.html')
+
+@app.route('/register')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat')
 def index():
     return render_template('index.html')
