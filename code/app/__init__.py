274eb6e91e634c903d06beb3e6a36ac8c3291b39
jacob gathercole <gathercole3@users.noreply.github.com>
2019-03-27 18:05:50 +0000

    Update __init__.py

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -5,7 +5,3 @@ print("run db")
 from app import database
 print("run routes")
 from app import routes
-
-if __name__ == '__main__':
-        app.static_folder = 'static'
-        app.run(debug=True,host='0.0.0.0')
