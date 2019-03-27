da978a3762b48db17a2bf45ba29df8491b6e8df9
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2019-03-27 20:34:06 +0000

    working

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -6,6 +6,6 @@ from app import database
 print("run routes")
 from app import routes
 
-if __name__ == '__main__':
-        app.static_folder = 'static'
+if __name__ == "__main__":
+        app.static_folder = 'app/static'
         app.run(debug=True,host='0.0.0.0')
--- code/chat.py
+++ code/chat.py
@@ -1,9 +1,4 @@
 from app import app
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/gathercole3-patch-1
-if __name__ == '__main__':
-        app.static_folder = 'app/static'
-        app.run(debug=True,host='0.0.0.0')
+
