ecc66fe3c613084b18f07888557ffe5991ecf3b9
mhfarrell <matthew.farrell@students.plymouth.ac.uk>
2019-03-27 20:22:43 +0000

    ignoring app.run() bug

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -5,3 +5,7 @@ print("run db")
 from app import database
 print("run routes")
 from app import routes
+
+if __name__ == '__main__':
+        app.static_folder = 'app/static'
+        app.run(debug=True,host='0.0.0.0')
