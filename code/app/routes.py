1b9eb08148d364d32bc57472988b2ce878d3b8bf
Matt Farrell <matthew.h.farrell@gmail.com>
2026-03-30 18:21:46 +0100

    UI refresh and security hardening
    - Redesign: slate/orange colour scheme, Inter font, flexbox layout
    - Fix layout: replace float-based layout with proper flexbox
    - Add favicon (SVG chat bubble)
    - Add footer with author info and links
    - Auto-select first chat on page load
    - Fix contact click handler to delegate to entire row
    - Security: escape HTML client-side to prevent stored XSS
    - Security: escape regex in search to prevent NoSQL injection
    - Security: use server-side session username for message sender
    - Security: add login_required decorator to API routes
    - Security: use random secret key fallback instead of hardcoded string
    - Remove dead database.py import from app init
    - Fix html-sanitizer version in requirements.txt

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,5 +1,7 @@
 import os
+import re
 import sys, time, bcrypt, pymongo
+from functools import wraps
 from threading import Lock
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from app import app
@@ -12,12 +14,19 @@ async_mode = None
 mongo_uri = os.environ.get('MONGO_URI', 'mongodb://admin:Admin123@localhost:27017/chatdatabase?authSource=admin')
 c = MongoClient(mongo_uri)
 db = c.chatdatabase
-#make this a hashed version of something unique to the user
-app.secret_key = os.environ.get('SECRET_KEY', 'shush_its_secret')
+app.secret_key = os.environ.get('SECRET_KEY', os.urandom(32))
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 sanitizer = Sanitizer()
+
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated
 
 def loadChat():
     myQuery = { '$or': [ { 'recipient': session['username'] }, { 'sender': session['username'] } ] }
@@ -55,13 +64,15 @@ def createChat(username):
 
 def loadContact(search):
     print('search text is: ' + search)
+    # Escape regex special characters to prevent ReDoS / regex injection
+    safe_search = re.escape(search)
     myQuery = {'username' : {'$ne' : session['username']},
            '$and' : [{'$or' : [
-                 {'username' : {'$regex' : search, '$options': 'i'}},
-                 {'surname' : {'$regex' : search, '$options': 'i'}},
-                 {'firstName' : {'$regex' : search, '$options': 'i'}},
-                 {'email' : {'$regex' : search, '$options': 'i'}},
-                 {'company' : {'$regex' : search, '$options': 'i'}}]}]}
+                 {'username' : {'$regex' : safe_search, '$options': 'i'}},
+                 {'surname' : {'$regex' : safe_search, '$options': 'i'}},
+                 {'firstName' : {'$regex' : safe_search, '$options': 'i'}},
+                 {'email' : {'$regex' : safe_search, '$options': 'i'}},
+                 {'company' : {'$regex' : safe_search, '$options': 'i'}}]}]}
     cursor = db.users.find(myQuery)
     payload = []
     for doc in cursor:
@@ -106,18 +117,20 @@ def index():
                            Form='login-form', \
                            altForm='register-form')
 
-@app.route('/search', methods=['POST', 'GET'])
+@app.route('/search', methods=['POST'])
+@login_required
 def search():
-    name = request.form['name']
-    search = request.form['search']
+    name = request.form.get('name', '')
+    search = request.form.get('search', '')
     if name and search:
         return jsonify(loadContact(search))
 
     return jsonify({'error' : 'Missing data!'})
 
-@app.route('/newchat', methods=['POST', 'GET'])
+@app.route('/newchat', methods=['POST'])
+@login_required
 def newchat():
-    username = request.form['username']
+    username = request.form.get('username', '')
     if username:
         print("the username is: " + username)
         cChat = jsonify(createChat(username))
@@ -145,26 +158,39 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('username')
+    session.pop('username', None)
     return render_template('login.html', \
                            Form='login-form', \
                            altForm='register-form', \
                            loginMessage='You have been logged out!')
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST'])
 def register():
-    if request.method == 'POST':
-        users = db.users
-        #userCheck is checking database if the username from the new user exists already
-        userCheck = users.find_one({'username' : request.form['username']})
-        #If the username is unclaimed the new user can register
-        if userCheck is None:
-            hashPass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'username' : request.form['username'], 'password' : hashPass, 'firstName' : request.form['firstname'], 'surname' : request.form['surname'], 'email' : request.form['email'], 'company' : request.form['company']})
-            session['username'] = request.form['username']
-            return render_template('index.html',\
-                                   current_user=session['username'],\
-                                   chats=loadChat())
+    users = db.users
+    username = request.form.get('username', '').strip()
+    password = request.form.get('password', '')
+
+    if not username or not password:
+        return render_template('login.html', \
+                               altForm='login-form', \
+                               Form='register-form', \
+                               registerMessage='Username and password are required')
+
+    userCheck = users.find_one({'username' : username})
+    if userCheck is None:
+        hashPass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        users.insert_one({
+            'username' : username,
+            'password' : hashPass,
+            'firstName' : request.form.get('firstname', ''),
+            'surname' : request.form.get('surname', ''),
+            'email' : request.form.get('email', ''),
+            'company' : request.form.get('company', '')
+        })
+        session['username'] = username
+        return render_template('index.html',\
+                               current_user=session['username'],\
+                               chats=loadChat())
 
     return render_template('login.html', \
                            altForm='login-form', \
@@ -197,17 +223,20 @@ def leave(message):
          {'data': 'Left Room: ' + ', '.join(rooms()),
           'count': session['receive_count']})
 
-#<script>alert("Hello! I am an alert box!!");</script>
 @socketio.on('sendMessage', namespace='/')
 def sendMessage(message):
+    # Use server-side session username, not client-supplied sender
+    if 'username' not in session:
+        return
+    sender = session['username']
     session['receive_count'] = session.get('receive_count', 0) + 1
     newID = "msg" + str(db.chat.count_documents({}) + 1)
     sanatiseMsg = sanitizer.sanitize(message['data'])
     if sanatiseMsg != "":
-        myDict = {"msgID": newID , "chatID" : message['room'], "recipient" : message['recipient'], "sender": message['sender'], "datetime": int(time.time()) , "data": sanatiseMsg}
+        myDict = {"msgID": newID , "chatID" : message['room'], "recipient" : message['recipient'], "sender": sender, "datetime": int(time.time()) , "data": sanatiseMsg}
         db.chat.insert_one(myDict)
         emit('my_response',
-             {'data': sanatiseMsg, 'username': message['sender'], 'count': session['receive_count']},
+             {'data': sanatiseMsg, 'username': sender, 'count': session['receive_count']},
              room=message['room'])
 
 
--- code/app/static/js/index.js
+++ code/app/static/js/index.js
@@ -13,144 +13,149 @@ $('#addcontact').click(function(){
 	}
 });
 
-        $(document).ready(function() {
-            namespace = '/';
-			var currentRoom = null;
-			var activeRoom = null;
-			var activeUser = null;
-            var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
-            socket.on('connect', function() {
-                socket.emit('myConnect', {data: $('#yourUsername').text()});
-            });
+// Escape HTML to prevent XSS when inserting user content into the DOM
+function escapeHtml(str) {
+	if (!str) return '';
+	var div = document.createElement('div');
+	div.appendChild(document.createTextNode(str));
+	return div.innerHTML;
+}
 
-            socket.on('my_response', function(msg) {
-				if(msg.username == null){
-					console.log('Received #' + msg.count + ' : ' + msg.data);
-				}else{
-					if(activeUser == msg.username){
-						$('<li class="sent"><img src="/static/images/placeholder.png" alt="" title="'+ msg.username +'" /><p>' + msg.data + '</p></li>').appendTo($('.messages ul'));
-						$('.message-input input').val(null);
-						$('.contact.active .preview').html('<i class="fa fa-comment-o fa-fw" aria-hidden="true"></i><span>' + msg.username +': </span>' + msg.data);
-						$(".messages").animate({ scrollTop: $(document).height() }, "fast");
-					}else{
-						$('<li class="replies"><img src="/static/images/placeholder.png" alt="" title="'+ msg.username +'" /><p>' + msg.data + '</p></li>').appendTo($('.messages ul'));
-						$('.contact.active .preview').html('<i class="fa fa-comment-o fa-fw" aria-hidden="true"></i><span>' + msg.username +': </span>' + msg.data);
-						$(".messages").animate({ scrollTop: $(document).height() }, "fast");
-					}
+$(document).ready(function() {
+	var namespace = '/';
+	var currentRoom = null;
+	var activeRoom = null;
+	var activeUser = null;
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+
+	socket.on('connect', function() {
+		socket.emit('myConnect', {data: $('#yourUsername').text()});
+	});
+
+	socket.on('my_response', function(msg) {
+		if(msg.username == null){
+			console.log('Received #' + msg.count + ' : ' + msg.data);
+		}else{
+			var safeUser = escapeHtml(msg.username);
+			var safeData = escapeHtml(msg.data);
+			if(activeUser == msg.username){
+				$('<li class="sent"><img src="/static/images/placeholder.png" alt="" title="'+ safeUser +'" /><p>' + safeData + '</p></li>').appendTo($('.messages ul'));
+				$('.message-input input').val(null);
+				$('.contact.active .preview').html('<i class="fa fa-comment-o fa-fw" aria-hidden="true"></i><span>' + safeUser +': </span>' + safeData);
+				$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+			}else{
+				$('<li class="replies"><img src="/static/images/placeholder.png" alt="" title="'+ safeUser +'" /><p>' + safeData + '</p></li>').appendTo($('.messages ul'));
+				$('.contact.active .preview').html('<i class="fa fa-comment-o fa-fw" aria-hidden="true"></i><span>' + safeUser +': </span>' + safeData);
+				$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+			}
+		}
+	});
+
+	// Contact selection - delegate to entire .contact li
+	$(document).on('click', '.contact', function(){
+		var liID = $(this).attr('id');
+		console.log('contact selected', liID);
+		if (activeRoom == null){
+			roomJoin(liID);
+		}else{
+			$('#'+activeRoom).removeClass('contact active').addClass('contact');
+			roomJoin(liID);
+		}
+	});
+
+	// Search result selection
+	$(document).on('click', '.searchedName', function(event){
+		var liID = $(event.target).closest('li').attr('id');
+		console.log('ajax');
+		$.ajax({
+			data : {
+				username : liID
+			},
+			type : 'POST',
+			url : '/newchat'
+		})
+		.done(function(data) {
+			console.log('after ajax');
+			if (data.error) {
+				console.log(data.error);
+			}
+			else {
+				window.location.href = '/';
+			}
+		});
+		event.preventDefault();
+	});
+
+	function joinCode(i){
+		currentRoom = $('#chat'+i).text();
+		activeRoom = i;
+		activeUser = $('#yourUsername').text();
+		$('#curContact').text($('#user'+i).text());
+		$('#'+i).attr('class', 'contact active');
+		socket.emit('join', {room: currentRoom, username: activeUser});
+	}
+
+	function leaveCode(i){
+		socket.emit('leave', {room: currentRoom});
+		$('#curContact').text(null);
+		$('#chatMsg').empty();
+		$('#'+i).removeClass('contact active').addClass('contact');
+	}
+
+	function roomJoin(i){
+		if (currentRoom == null){
+			joinCode(i);
+		}else{
+			leaveCode(i);
+			joinCode(i);
+		}
+		return false;
+	}
+
+	document.getElementById('contactSearch').onkeydown = function(e){
+		if(e.keyCode == 13){
+			console.log('ajax');
+			$.ajax({
+				data : {
+					name : $('#yourUsername').text(),
+					search : $('#contSearch').val()
+				},
+				type : 'POST',
+				url : '/search'
+			})
+			.done(function(data) {
+				console.log('after ajax');
+				if (data.error) {
+					console.log(data.error);
 				}
-            });
-
-			//
-			//individual user chats
-			//
-			$(document).on('click', function(event){
-				var liID = $(event.target).closest('li').attr('id');
-				console.log(liID);
-				if($(event.target).hasClass('contactName')){
-					console.log("contact selected")
-					if (activeRoom == null){
-						roomJoin(liID);
-					}else{
-						$('#'+activeRoom).removeClass('contact active').addClass('contact');
-						roomJoin(liID);
+				else {
+					console.log(data);
+					for(var i in data){
+						var safeUsername = escapeHtml(data[i].username);
+						$('<li class="contactResult" id="'+ safeUsername +'"><div class="wrap"><div class="meta"><p class="name searchedName">'+ safeUsername +'</p></div></div></li>').appendTo($('.contactResults ul'));
 					}
-				}else if($(event.target).hasClass('searchedName')){
-
-					console.log('ajax');
-					$.ajax({
-						data : {
-							username : liID
-						},
-						type : 'POST',
-						url : '/newchat'
-						})
-							.done(function(data) {
-						console.log('after ajax');
-						if (data.error) {
-							console.log(data.error)
-						}
-						else {
-							window.location.href = '/';
-						}
-					});
-					event.preventDefault();
-
 				}
 			});
+			$("#contSearch").prop("value", "");
+			e.preventDefault();
+		}
+	};
 
-			function joinCode(i){
-				currentRoom = $('#chat'+i).text();
-				activeRoom = i;
-				activeUser = $('#yourUsername').text();
-				$('#curContact').text($('#user'+i).text());
-				//toggle class later with css
-				$('#'+i).attr('class', 'contact active');
-				socket.emit('join', {room: currentRoom, username: activeUser});
-				return;
-			}
+	$('form#sendMessage').submit(function(event) {
+		console.log('room: ' + currentRoom + ', username: ' + activeUser);
+		socket.emit('sendMessage', {room: currentRoom, data: $('#roomMessage').val(), sender: activeUser, recipient: $('#user'+activeRoom).text()});
+		$("#roomMessage").prop("value", "");
+		return false;
+	});
 
-			function leaveCode(i){
-				socket.emit('leave', {room: currentRoom});
-				$('#curContact').text(null);
-				$('#chatMsg').empty();
-				//toggle class later
-				$('#'+i).removeClass('contact active').addClass('contact');
-				return;
-			}
+	$('form#disconnect').submit(function(event) {
+		socket.emit('disconnect_request');
+		return false;
+	});
 
-			//pointless but seems to not work without ?!?!
-			function roomJoin(i){
-				if (currentRoom == null){
-					joinCode(i);
-					return false;
-				}else{
-					leaveCode(i);
-					joinCode(i);
-					return false;
-				}
-			}
-
-			document.getElementById('contactSearch').onkeydown = function(e){
-				if(e.keyCode == 13){
-					console.log('ajax');
-					$.ajax({
-						data : {
-							name : $('#yourUsername').text(),
-							search : $('#contSearch').val()
-						},
-						type : 'POST',
-						url : '/search'
-						})
-							.done(function(data) {
-						console.log('after ajax');
-						if (data.error) {
-							console.log(data.error)
-						}
-						else {
-							console.log(data);
-							for(i in data){
-								console.log(i);
-								$('<li class="contactResult" id="'+ data[i].username +'"><div class="wrap"><div class="meta"><p class="name searchedName">'+ data[i].username +'</p></div></div></li>').appendTo($('.contactResults ul'));
-							console.log(data[i].username);
-							}
-						}
-					});
-					$("#contSearch").prop("value", "");
-					e.preventDefault();
-				}
-			};
-
-
-            $('form#sendMessage').submit(function(event) {
-				console.log('room: ' + currentRoom + ', username: ' + activeUser);
-                socket.emit('sendMessage', {room: currentRoom, data: $('#roomMessage').val(), sender: activeUser, recipient: $('#user'+activeRoom).text()});
-				$("#roomMessage").prop("value", "");
-                return false;
-            });
-
-			//part of logout code
-            $('form#disconnect').submit(function(event) {
-                socket.emit('disconnect_request');
-                return false;
-            });
-        });
+	// Auto-select the first chat on load
+	var firstContact = $('.contacts ul li.contact').first();
+	if (firstContact.length) {
+		roomJoin(firstContact.attr('id'));
+	}
+});
